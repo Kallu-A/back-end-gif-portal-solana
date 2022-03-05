823cc44d3d698b619b2a0535de5fb1ca69dda7e1
Kallu-A <lucas.aries@protonmail.com>
2022-03-05 22:32:31 +0100

    update test with mocha

---
+++ README.md
@@ -0,0 +1,6 @@
+# Solana program
+
+Litle project to learn developping on the solana blockchain
+Tools used : Anchor, Rust, BuildSpace
+
+Go see at : https://github.com/Kallu-A/front-end-gif-portal-solana for the web app and the demo of the code 
--- tests/gifportal.js
+++ tests/gifportal.js
@@ -1,132 +1,95 @@
 const anchor = require('@project-serum/anchor');
-
+const assert = require('assert');
 const { SystemProgram } = anchor.web3;
 
-const main = async() => {
+describe('Testing Gif-Portal', () => {
   console.log("🚀 Starting test...")
-
   const provider = anchor.Provider.env();
   anchor.setProvider(anchor.Provider.env());
   const program = anchor.workspace.Gifportal;
-
-  // Create an account keypair for our program to use.
   const baseAccount = anchor.web3.Keypair.generate();
+  let account;
 
-  // Call start_stuff_off, pass it the params it needs!
-  let tx = await program.rpc.startStuffOff({
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId,
-    },
-    signers: [baseAccount],
-  });
+  it("Add gif UpVote Remove", async () => {
 
-  console.log("📝 Your transaction signature", tx);
-
-  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF Count', account.totalGifs.toString())
-
-  // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
-  await program.rpc.addGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", {
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-    },
-  });
-  
-  // Call the account.
-  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF Count', account.totalGifs.toString())
-
-  // Access gif_list on the account!
-  console.log('👀 GIF List', account.gifList)
-
-  console.log("Add up Vote");
-
-  await program.rpc.addUpvote("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", provider.wallet.publicKey, {
+    await program.rpc.startStuffOff({
       accounts: {
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
       },
-  });
-  
-  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF List', account.gifList)
-
-  try {
-    await program.rpc.addUpvote("wronggifdontexist", provider.wallet.publicKey, {
-      accounts: {
-        baseAccount: baseAccount.publicKey,
-        user: provider.wallet.publicKey,
-      },
+      signers: [baseAccount],
     });
-    console.log("Oops. error throw not as expected ! ")
-    process.exit(1);
-  } catch( e ) {
-    console.log("Error throw as expected ");
-  }
+  
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
 
-  await program.rpc.addGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy2.gif", {
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-    },
-  });
-
-  try {
+    // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
     await program.rpc.addGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", {
       accounts: {
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
       },
     });
-    console.log("Oops. error throw not as expected ! ")
-    process.exit(1);
-  } catch( e ) {
-    console.log("Error throw as expected ");
-  }
 
-  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF List', account.gifList)
+    await program.rpc.addUpvote("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", provider.wallet.publicKey, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+    });
+    
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    assert.ok(account.totalGifs == 1);
 
-  await program.rpc.removeGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", {
-    accounts: {
-      baseAccount: baseAccount.publicKey,
-      user: provider.wallet.publicKey,
-    },
-  });
-  console.log("remove gif");
-  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-  console.log('👀 GIF List', account.gifList)
+    try {
+      await program.rpc.addUpvote("wronggifdontexist", provider.wallet.publicKey, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      process.exit(1);
+    } catch( e ) { }
+  
+    await program.rpc.addGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy2.gif", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    });
 
-  try {
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    assert.ok(account.totalGifs == 2);
+
+    try {
+      await program.rpc.addGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      process.exit(1);
+    } catch( e ) { }
+  
     await program.rpc.removeGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", {
       accounts: {
         baseAccount: baseAccount.publicKey,
         user: provider.wallet.publicKey,
       },
     });
-    console.log("Oops. error throw not as expected ! ")
-    process.exit(1);
-  } catch( e ) {
-    console.log("Error throw as expected ");
-  }
 
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    assert.ok(account.totalGifs == 1);
 
-
-
-  console.log("\n\n🚀 Test working ! ")
-}
-
-const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-runMain();
+    try {
+      await program.rpc.removeGif("https://media.giphy.com/media/3ornjPteRwwUdSWifC/giphy.gif", {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      process.exit(1);
+    } catch( e ) {}
+  });
+  
+});
